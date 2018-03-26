826f76d96a6224f8cc1fda25935e34d6ec616fb5
Tom Jorquera <tom@jorquera.net>
2018-03-26 22:33:42 +0200

    fix screwed up indent
    No content change

--- js/main.js
+++ js/main.js
@@ -4,166 +4,166 @@ $('body').removeClass('js-disabled');
 
 function SwarmControl(canvas, buffer) {
 
-    var that = this;
+  var that = this;
 
-    SwarmControl.prototype.start = function() {
+  SwarmControl.prototype.start = function() {
 
-        canvas.width = window.innerWidth;
-        canvas.height = window.innerHeight;
-        canvas.getContext('2d').fillStyle = 'white';
-        canvas.getContext('2d').fillRect(0, 0, canvas.width , canvas.height);
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    canvas.getContext('2d').fillStyle = 'white';
+    canvas.getContext('2d').fillRect(0, 0, canvas.width , canvas.height);
 
-        buffer.width = window.innerWidth;
-        buffer.height = window.innerHeight;
-        buffer.getContext('2d').fillStyle = 'white';
-        buffer.getContext('2d').fillRect(0, 0, buffer.width , buffer.height);
+    buffer.width = window.innerWidth;
+    buffer.height = window.innerHeight;
+    buffer.getContext('2d').fillStyle = 'white';
+    buffer.getContext('2d').fillRect(0, 0, buffer.width , buffer.height);
 
-        canvas.style.display="block";
-        canvas.style.opacity=1;
-        canvas.filter = "alpha(opacity=100);"; /* For IE8 and earlier */
+    canvas.style.display="block";
+    canvas.style.opacity=1;
+    canvas.filter = "alpha(opacity=100);"; /* For IE8 and earlier */
 
-        // FPS monitoring
-        var FPSbound = 15;
-        var timeUnderFPSBound = 0;
-        var fps = {
-            startTime : 0,
-            frameNumber : 0,
-            getFPS : function(){
-                this.frameNumber++;
-                var d = new Date().getTime();
-                var currentTime = ( d - this.startTime ) / 1000;
-                var result = Math.floor( ( this.frameNumber / currentTime ) );
+    // FPS monitoring
+    var FPSbound = 15;
+    var timeUnderFPSBound = 0;
+    var fps = {
+      startTime : 0,
+      frameNumber : 0,
+      getFPS : function(){
+        this.frameNumber++;
+        var d = new Date().getTime();
+        var currentTime = ( d - this.startTime ) / 1000;
+        var result = Math.floor( ( this.frameNumber / currentTime ) );
 
-                if( currentTime > 1 ){
-                    this.startTime = new Date().getTime();
-                    this.frameNumber = 0;
-                }
+        if( currentTime > 1 ){
+          this.startTime = new Date().getTime();
+          this.frameNumber = 0;
+        }
 
-                // update time under FPS bound
-                if(result >= FPSbound) {
-                    timeUnderFPSBound = 0;
-                } else {
-                    timeUnderFPSBound++;
-                }
-                return result;
+        // update time under FPS bound
+        if(result >= FPSbound) {
+          timeUnderFPSBound = 0;
+        } else {
+          timeUnderFPSBound++;
+        }
+        return result;
 
-            }
-        };
-
-        var timer;
-        var swarm = new Swarm(canvas, buffer);
-
-        var step = function(){
-            swarm.step();
-
-            //check FPS
-            fps.getFPS();
-            if (timeUnderFPSBound >= 500) {
-                // not enough performances, better stop
-                that.stop(timer);
-            }
-        };
-
-        timer = setInterval(step, 50);
-
-        SwarmUI.inactive();
+      }
     };
 
-    SwarmControl.prototype.stop = function(timer) {
-        clearInterval(timer);
-        canvas.style.display="none";
-        canvas.style.opacity=0;
-        canvas.filter = "alpha(opacity=0);"; /* For IE8 and earlier */
-        SwarmUI.error();
+    var timer;
+    var swarm = new Swarm(canvas, buffer);
+
+    var step = function(){
+      swarm.step();
+
+      //check FPS
+      fps.getFPS();
+      if (timeUnderFPSBound >= 500) {
+        // not enough performances, better stop
+        that.stop(timer);
+      }
     };
+
+    timer = setInterval(step, 50);
+
+    SwarmUI.inactive();
+  };
+
+  SwarmControl.prototype.stop = function(timer) {
+    clearInterval(timer);
+    canvas.style.display="none";
+    canvas.style.opacity=0;
+    canvas.filter = "alpha(opacity=0);"; /* For IE8 and earlier */
+    SwarmUI.error();
+  };
 }
 
 function SwarmUI() {
 
-    var isActive = false;
-    var error = false;
+  var isActive = false;
+  var error = false;
 
-    SwarmUI.prototype.inactive = function() {
-        isActive = false;
-        error = false;
+  SwarmUI.prototype.inactive = function() {
+    isActive = false;
+    error = false;
 
-        $(".main").removeClass('moved');
-        $("#back").removeClass('moved');
+    $(".main").removeClass('moved');
+    $("#back").removeClass('moved');
 
-        $(".bg-button").removeClass('moved');
-        $(".bg-button").removeClass('sec-button');
-        $(".bg-button").attr("onclick","SwarmUI.toggleActive();");
-        $(".info-button").removeClass('moved');
-        $(".info-button").removeClass('sec-button');
-        $(".info-button").attr("onclick","$('#boids-info').toggle();");
-        $(".error-button").removeClass('moved');
-        $(".error-button").attr("onclick","");
-        $(".reload-button").removeClass('moved');
-        $(".reload-button").attr("onclick","");
+    $(".bg-button").removeClass('moved');
+    $(".bg-button").removeClass('sec-button');
+    $(".bg-button").attr("onclick","SwarmUI.toggleActive();");
+    $(".info-button").removeClass('moved');
+    $(".info-button").removeClass('sec-button');
+    $(".info-button").attr("onclick","$('#boids-info').toggle();");
+    $(".error-button").removeClass('moved');
+    $(".error-button").attr("onclick","");
+    $(".reload-button").removeClass('moved');
+    $(".reload-button").attr("onclick","");
 
-        if($('#boids-error').is(":visible")){
-            $('#boids-error').hide();
-            $('#boids-info').show();
-        }
-    };
+    if($('#boids-error').is(":visible")){
+      $('#boids-error').hide();
+      $('#boids-info').show();
+    }
+  };
 
-    SwarmUI.prototype.active = function() {
-        isActive = true;
-        error = false;
+  SwarmUI.prototype.active = function() {
+    isActive = true;
+    error = false;
 
-        $(".main").addClass('moved');
-        $("#back").addClass('moved');
+    $(".main").addClass('moved');
+    $("#back").addClass('moved');
 
-        $(".bg-button").addClass('moved');
-        $(".bg-button").removeClass('sec-button');
-        $(".bg-button").attr("onclick","SwarmUI.toggleActive();");
-        $(".info-button").addClass('moved');
-        $(".info-button").removeClass('sec-button');
-        $(".info-button").attr("onclick","$('#boids-info').toggle();");
-        $(".error-button").removeClass('moved');
-        $(".error-button").attr("onclick","");
-        $(".reload-button").removeClass('moved');
-        $(".reload-button").attr("onclick","");
+    $(".bg-button").addClass('moved');
+    $(".bg-button").removeClass('sec-button');
+    $(".bg-button").attr("onclick","SwarmUI.toggleActive();");
+    $(".info-button").addClass('moved');
+    $(".info-button").removeClass('sec-button');
+    $(".info-button").attr("onclick","$('#boids-info').toggle();");
+    $(".error-button").removeClass('moved');
+    $(".error-button").attr("onclick","");
+    $(".reload-button").removeClass('moved');
+    $(".reload-button").attr("onclick","");
 
-        if($('#boids-error').is(":visible")){
-            $('#boids-error').hide();
-            $('#boids-info').show();
-        }
-    };
+    if($('#boids-error').is(":visible")){
+      $('#boids-error').hide();
+      $('#boids-info').show();
+    }
+  };
 
-    SwarmUI.prototype.error = function() {
-        isActive = false;
-        error = true;
+  SwarmUI.prototype.error = function() {
+    isActive = false;
+    error = true;
 
-        $(".main").removeClass('moved');
-        $("#back").removeClass('moved');
+    $(".main").removeClass('moved');
+    $("#back").removeClass('moved');
 
-        $(".bg-button").removeClass('moved');
-        $(".bg-button").addClass('sec-button');
-        $(".bg-button").attr("onclick","");
-        $(".info-button").removeClass('moved');
-        $(".info-button").addClass('sec-button');
-        $(".info-button").attr("onclick","");
-        $(".error-button").addClass('moved');
-        $(".error-button").attr("onclick","$('#boids-error').toggle();");
-        $(".reload-button").addClass('moved');
-        $(".reload-button").attr("onclick","SwarmControl.start();");
+    $(".bg-button").removeClass('moved');
+    $(".bg-button").addClass('sec-button');
+    $(".bg-button").attr("onclick","");
+    $(".info-button").removeClass('moved');
+    $(".info-button").addClass('sec-button');
+    $(".info-button").attr("onclick","");
+    $(".error-button").addClass('moved');
+    $(".error-button").attr("onclick","$('#boids-error').toggle();");
+    $(".reload-button").addClass('moved');
+    $(".reload-button").attr("onclick","SwarmControl.start();");
 
-        if($('#boids-info').is(":visible")){
-            $('#boids-info').hide();
-            $('#boids-error').show();
-        } else {
-            $('#boids-error').hide();
-        }
-    };
+    if($('#boids-info').is(":visible")){
+      $('#boids-info').hide();
+      $('#boids-error').show();
+    } else {
+      $('#boids-error').hide();
+    }
+  };
 
-    SwarmUI.prototype.toggleActive = function() {
-        isActive ? this.inactive() : this.active();
-    };
+  SwarmUI.prototype.toggleActive = function() {
+    isActive ? this.inactive() : this.active();
+  };
 
-    SwarmUI.prototype.toggleError = function() {
-        error ? this.inactive() : this.error();
-    };
+  SwarmUI.prototype.toggleError = function() {
+    error ? this.inactive() : this.error();
+  };
 }
 
 var SwarmControl;
@@ -171,39 +171,39 @@ var SwarmUI;
 
 $(document).ready( function() {
 
-    // Some Javascript for better tabs
+  // Some Javascript for better tabs
 
-    // Show the tab corresponding with the hash in the URL, or the first tab.
-    var showTabFromHash = function() {
-        var hash = window.location.hash;
-        var selector = hash ? 'a[href="' + hash + '"]' : 'li.active > a';
-        $(selector).tab('show');
-    };
-    // Set the correct tab when the page loads
-    showTabFromHash();
-    window.scrollTo(0, 0);
+  // Show the tab corresponding with the hash in the URL, or the first tab.
+  var showTabFromHash = function() {
+    var hash = window.location.hash;
+    var selector = hash ? 'a[href="' + hash + '"]' : 'li.active > a';
+    $(selector).tab('show');
+  };
+  // Set the correct tab when the page loads
+  showTabFromHash();
+  window.scrollTo(0, 0);
 
-    // Set the correct tab when a user uses their back/forward button
-    window.addEventListener('hashchange', showTabFromHash, false);
+  // Set the correct tab when a user uses their back/forward button
+  window.addEventListener('hashchange', showTabFromHash, false);
 
-    // Change the URL when tabs are clicked
-    $('#mainTab a').on('click', function(e) {
-        history.pushState(null, null, this.href);
-    });
+  // Change the URL when tabs are clicked
+  $('#mainTab a').on('click', function(e) {
+    history.pushState(null, null, this.href);
+  });
 
-    // we create a canvas and a buffer (better performances)
-    var canvas = document.createElement('canvas');
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    canvas.getContext('2d').fillStyle = 'white';
-    canvas.getContext('2d').fillRect(0, 0, canvas.width , canvas.height);
-    document.getElementById("back").appendChild(canvas);
+  // we create a canvas and a buffer (better performances)
+  var canvas = document.createElement('canvas');
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  canvas.getContext('2d').fillStyle = 'white';
+  canvas.getContext('2d').fillRect(0, 0, canvas.width , canvas.height);
+  document.getElementById("back").appendChild(canvas);
 
-    var buffer = document.createElement('canvas');
+  var buffer = document.createElement('canvas');
 
-    SwarmUI = new SwarmUI();
-    SwarmControl = new SwarmControl(canvas, buffer);
-    //let's rock!
-    SwarmControl.start();
+  SwarmUI = new SwarmUI();
+  SwarmControl = new SwarmControl(canvas, buffer);
+  //let's rock!
+  SwarmControl.start();
 
 });
--- js/my-boids.js
+++ js/my-boids.js
@@ -1,239 +1,239 @@
 function Swarm(canvas, buffer) {
 
-    var context = buffer.getContext('2d');
+  var context = buffer.getContext('2d');
 
-    // some margin for the scrollbar
-    var width = window.innerWidth - 20;
-    var height = window.innerHeight;
+  // some margin for the scrollbar
+  var width = window.innerWidth - 20;
+  var height = window.innerHeight;
 
-    var NB_BOIDS = 50;
-    var BOID_FOV = 50;
-    var BOID_SPEED = 2;
-    var BOID_INERTIA = 0.8;
+  var NB_BOIDS = 50;
+  var BOID_FOV = 50;
+  var BOID_SPEED = 2;
+  var BOID_INERTIA = 0.8;
 
-    var BOID_FLEE = 250;
-    var BOID_FLEE_RATIO = 0.66;
+  var BOID_FLEE = 250;
+  var BOID_FLEE_RATIO = 0.66;
 
-    var flee = 0;
+  var flee = 0;
 
-    var SEP_COEFF = 1 * (1 - BOID_INERTIA);
-    var ALI_COEFF = 1 * (1 - BOID_INERTIA);
-    var COH_COEFF = 1 * (1 - BOID_INERTIA);
+  var SEP_COEFF = 1 * (1 - BOID_INERTIA);
+  var ALI_COEFF = 1 * (1 - BOID_INERTIA);
+  var COH_COEFF = 1 * (1 - BOID_INERTIA);
 
-    var BORDER_PADDING = 8;
+  var BORDER_PADDING = 8;
 
-    var radius = 6;
-    var queueLength = radius * 2.5;
+  var radius = 6;
+  var queueLength = radius * 2.5;
 
-    var boids = []; // the boids
-    for(var i = 0; i < NB_BOIDS; i++) {
-        boids.push(new Boid());
+  var boids = []; // the boids
+  for(var i = 0; i < NB_BOIDS; i++) {
+    boids.push(new Boid());
+  }
+
+  function Boid() {
+    this.pos = vec2.fromValues(Math.random() * width,
+                               Math.random() * height);
+    var vx = -2 + 4 * Math.random();
+    var vy = -2 + 4 * Math.random();
+
+    // ensure acceptable min speed
+    if(Math.abs(vx) < 0.5) vx = vx/Math.abs(vx) * 0.5;
+    if(Math.abs(vy) < 0.5) vy = vy/Math.abs(vy) * 0.5;
+
+    this.velocity = vec2.fromValues(vx, vy);
+  }
+
+  Boid.prototype.undraw = function(ctx) {
+    // remove old boid
+    ctx.fillStyle = 'white';
+    ctx.strokeStyle = 'white';
+    ctx.beginPath();
+    // round coordinates to integer (huge speedup on canvas)
+    ctx.arc((this.pos[0] + 0.5)|0,
+            (this.pos[1] + 0.5)|0,
+            BOID_SPEED * queueLength + radius, 0, 2 *
+            Math.PI);
+    ctx.fill();
+  };
+
+  Boid.prototype.draw = function(ctx) {
+
+    ctx.fillStyle = 'blue';
+    ctx.strokeStyle = 'blue';
+
+    var b = vec2.fromValues(-1* this.velocity[1], this.velocity[0]);
+    vec2.scale(b, b, radius / Math.sqrt(b[0]*b[0] + b[1]*b[1]));
+    var b0 = vec2.add(vec2.create(), this.pos, b);
+    var b1 = vec2.sub(vec2.create(), this.pos, b);
+    var queue = vec2.scale(vec2.create(), this.velocity, -1 * queueLength);
+
+    if(vec2.length(queue) < queueLength) {
+      // ensure the boid will not be too small
+      vec2.scale(queue, vec2.normalize(queue, queue), queueLength);
     }
 
-    function Boid() {
-        this.pos = vec2.fromValues(Math.random() * width,
-                                   Math.random() * height);
-        var vx = -2 + 4 * Math.random();
-        var vy = -2 + 4 * Math.random();
+    //place the queue at the position of the boid
+    vec2.add(queue, this.pos, queue);
 
-        // ensure acceptable min speed
-        if(Math.abs(vx) < 0.5) vx = vx/Math.abs(vx) * 0.5;
-        if(Math.abs(vy) < 0.5) vy = vy/Math.abs(vy) * 0.5;
+    ctx.beginPath();
+    ctx.moveTo(queue[0], queue[1]);
+    ctx.lineTo(b0[0], b0[1]);
+    ctx.lineTo(b1[0], b1[1]);
+    ctx.fill();
 
-        this.velocity = vec2.fromValues(vx, vy);
+  };
+
+  Boid.prototype.checkBorders = function() {
+    var correction = vec2.create();
+
+    if(this.pos[0] < BORDER_PADDING) {
+      correction[0] = 1;
+    } else if (this.pos[0] > width - BORDER_PADDING) {
+      correction[0] = -1;
     }
 
-    Boid.prototype.undraw = function(ctx) {
-        // remove old boid
-        ctx.fillStyle = 'white';
-        ctx.strokeStyle = 'white';
-        ctx.beginPath();
-        // round coordinates to integer (huge speedup on canvas)
-        ctx.arc((this.pos[0] + 0.5)|0,
-                (this.pos[1] + 0.5)|0,
-                BOID_SPEED * queueLength + radius, 0, 2 *
-                Math.PI);
-        ctx.fill();
-    };
+    if(this.pos[1] < BORDER_PADDING) {
+      correction[1] = 1;
+    } else if (this.pos[1] > height - BORDER_PADDING) {
+      correction[1] = -1;
+    }
 
-    Boid.prototype.draw = function(ctx) {
+    // add corrections to steer the boid inside the borders
+    vec2.add(this.velocity, this.velocity, correction);
+    vec2.limit(this.velocity, this.velocity, BOID_SPEED);
+  };
 
-        ctx.fillStyle = 'blue';
-        ctx.strokeStyle = 'blue';
+  Boid.prototype.step = function() {
 
-        var b = vec2.fromValues(-1* this.velocity[1], this.velocity[0]);
-        vec2.scale(b, b, radius / Math.sqrt(b[0]*b[0] + b[1]*b[1]));
-        var b0 = vec2.add(vec2.create(), this.pos, b);
-        var b1 = vec2.sub(vec2.create(), this.pos, b);
-        var queue = vec2.scale(vec2.create(), this.velocity, -1 * queueLength);
+    var nbNeighbors = 0;
 
-        if(vec2.length(queue) < queueLength) {
-            // ensure the boid will not be too small
-            vec2.scale(queue, vec2.normalize(queue, queue), queueLength);
-        }
+    var separate = vec2.fromValues(0,0);
+    var align = vec2.fromValues(0, 0);
+    var cohere = vec2.fromValues(0,0);
 
-        //place the queue at the position of the boid
-        vec2.add(queue, this.pos, queue);
+    for(var i = 0; i < boids.length; i++) {
 
-        ctx.beginPath();
-        ctx.moveTo(queue[0], queue[1]);
-        ctx.lineTo(b0[0], b0[1]);
-        ctx.lineTo(b1[0], b1[1]);
-        ctx.fill();
+      var n = boids[i];
 
-    };
+      var dist = vec2.distance(this.pos, n.pos);
+      if(   dist <= BOID_FOV
+            && this != boids[i]
+        ) {
 
-    Boid.prototype.checkBorders = function() {
-        var correction = vec2.create();
+        nbNeighbors++;
 
-        if(this.pos[0] < BORDER_PADDING) {
-            correction[0] = 1;
-        } else if (this.pos[0] > width - BORDER_PADDING) {
-            correction[0] = -1;
-        }
+        // separate (non-linear relation)
+        var scaledSeparation = vec2.sub(vec2.create(),
+                                        this.pos, n.pos);
+        vec2.scale(scaledSeparation,
+                   scaledSeparation,
+                   1/(dist * dist));
+        vec2.add(separate, separate, scaledSeparation);
 
-        if(this.pos[1] < BORDER_PADDING) {
-            correction[1] = 1;
-        } else if (this.pos[1] > height - BORDER_PADDING) {
-            correction[1] = -1;
-        }
+        // align
+        vec2.add(align, align, n.velocity);
 
-        // add corrections to steer the boid inside the borders
-        vec2.add(this.velocity, this.velocity, correction);
-        vec2.limit(this.velocity, this.velocity, BOID_SPEED);
-    };
+        // cohere
+        vec2.add(cohere, cohere, n.pos);
 
-    Boid.prototype.step = function() {
+      }
+    }
 
-        var nbNeighbors = 0;
+    if(nbNeighbors > 0) {
 
-        var separate = vec2.fromValues(0,0);
-        var align = vec2.fromValues(0, 0);
-        var cohere = vec2.fromValues(0,0);
+      vec2.scale(align, align, 1/nbNeighbors);
+      vec2.normalize(align, align);
 
-        for(var i = 0; i < boids.length; i++) {
+      vec2.scale(cohere, cohere, 1/nbNeighbors);
+      vec2.sub(cohere, cohere, this.pos);
+      vec2.normalize(cohere, cohere);
 
-            var n = boids[i];
+      vec2.scale(separate, separate, 1/nbNeighbors);
+      vec2.normalize(separate, separate);
 
-            var dist = vec2.distance(this.pos, n.pos);
-            if(   dist <= BOID_FOV
-                  && this != boids[i]
-              ) {
+      if(nbNeighbors > boids.length * BOID_FLEE_RATIO && flee == 0) {
+        // things getting too crowded here, better split up
+        flee = BOID_FLEE;
+        COH_COEFF *= -1;
+      }
+      // scale and apply forces
+      vec2.scale(separate, separate, SEP_COEFF);
+      vec2.scale(align, align, ALI_COEFF);
+      vec2.scale(cohere, cohere, COH_COEFF);
 
-                  nbNeighbors++;
-
-                  // separate (non-linear relation)
-                  var scaledSeparation = vec2.sub(vec2.create(),
-                                                  this.pos, n.pos);
-                  vec2.scale(scaledSeparation,
-                             scaledSeparation,
-                             1/(dist * dist));
-                  vec2.add(separate, separate, scaledSeparation);
-
-                  // align
-                  vec2.add(align, align, n.velocity);
-
-                  // cohere
-                  vec2.add(cohere, cohere, n.pos);
-
-              }
-        }
-
-        if(nbNeighbors > 0) {
-
-            vec2.scale(align, align, 1/nbNeighbors);
-            vec2.normalize(align, align);
-
-            vec2.scale(cohere, cohere, 1/nbNeighbors);
-            vec2.sub(cohere, cohere, this.pos);
-            vec2.normalize(cohere, cohere);
-
-            vec2.scale(separate, separate, 1/nbNeighbors);
-            vec2.normalize(separate, separate);
-
-            if(nbNeighbors > boids.length * BOID_FLEE_RATIO && flee == 0) {
-                // things getting too crowded here, better split up
-                flee = BOID_FLEE;
-                COH_COEFF *= -1;
-            }
-            // scale and apply forces
-            vec2.scale(separate, separate, SEP_COEFF);
-            vec2.scale(align, align, ALI_COEFF);
-            vec2.scale(cohere, cohere, COH_COEFF);
-
-            var acc = vec2.fromValues(0, 0);
-            vec2.add(acc, acc, separate);
-            vec2.add(acc, acc, align);
-            vec2.add(acc, acc, cohere);
+      var acc = vec2.fromValues(0, 0);
+      vec2.add(acc, acc, separate);
+      vec2.add(acc, acc, align);
+      vec2.add(acc, acc, cohere);
 
 
-            // update velocity
-            vec2.add(this.velocity, this.velocity, acc);
-            vec2.limit(this.velocity, this.velocity, BOID_SPEED);
-        }
+      // update velocity
+      vec2.add(this.velocity, this.velocity, acc);
+      vec2.limit(this.velocity, this.velocity, BOID_SPEED);
+    }
 
-        // move
-        this.pos = vec2.add(vec2.create(), this.pos, this.velocity);
+    // move
+    this.pos = vec2.add(vec2.create(), this.pos, this.velocity);
 
-        this.checkBorders();
-    };
+    this.checkBorders();
+  };
 
-    // limit the magnitude of a vector without changing its direction
-    vec2.limit = function(out, a, n) {
-        var x = a[0],
-            y = a[1];
-        var len = x*x + y*y;
-        if (len > n * n) {
-            len = n / Math.sqrt(len);
-            out[0] = a[0] * len;
-            out[1] = a[1] * len;
-        }
-        return out;
-    };
+  // limit the magnitude of a vector without changing its direction
+  vec2.limit = function(out, a, n) {
+    var x = a[0],
+        y = a[1];
+    var len = x*x + y*y;
+    if (len > n * n) {
+      len = n / Math.sqrt(len);
+      out[0] = a[0] * len;
+      out[1] = a[1] * len;
+    }
+    return out;
+  };
 
-    Swarm.prototype.step = function() {
-        // adjust canvas if needed
-        if (buffer.width != window.innerWidth ||
-            buffer.height != window.innerHeight) {
-            // some margin for the scrollbar
-            width = window.innerWidth - 20;
-            height = window.innerHeight;
+  Swarm.prototype.step = function() {
+    // adjust canvas if needed
+    if (buffer.width != window.innerWidth ||
+        buffer.height != window.innerHeight) {
+      // some margin for the scrollbar
+      width = window.innerWidth - 20;
+      height = window.innerHeight;
 
-            buffer.width = window.innerWidth;
-            buffer.height = window.innerHeight;
+      buffer.width = window.innerWidth;
+      buffer.height = window.innerHeight;
 
-            context.fillStyle = 'white';
-            context.fillRect(0, 0, window.innerWidth, window.innerHeight);
+      context.fillStyle = 'white';
+      context.fillRect(0, 0, window.innerWidth, window.innerHeight);
 
-            canvas.width = window.innerWidth;
-            canvas.height = window.innerHeight;
-        }
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
+    }
 
-        var i, boid;
+    var i, boid;
 
-        // remove old boids
-        for (i = 0; i < boids.length; i++) {
-            boid = boids[i];
-            boid.undraw(context);
-        }
+    // remove old boids
+    for (i = 0; i < boids.length; i++) {
+      boid = boids[i];
+      boid.undraw(context);
+    }
 
-        // move boids
-        for (i = 0; i < boids.length; i++) {
-            boid = boids[i];
-            boid.step();
-            boid.draw(context);
-        }
+    // move boids
+    for (i = 0; i < boids.length; i++) {
+      boid = boids[i];
+      boid.step();
+      boid.draw(context);
+    }
 
-        if (flee > 0) {
-            flee--;
-            if (flee == 0) {
-                COH_COEFF *= -1;
-            }
-        }
+    if (flee > 0) {
+      flee--;
+      if (flee == 0) {
+        COH_COEFF *= -1;
+      }
+    }
 
-        // draw buffer into canvas
-        canvas.getContext('2d').drawImage(buffer,0,0);
+    // draw buffer into canvas
+    canvas.getContext('2d').drawImage(buffer,0,0);
 
-    };
+  };
 
 }
